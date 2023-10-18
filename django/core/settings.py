1219b621603951c17b42508ba4d141385d3ed862
Gary Godfrey <gary.god3@proton.me>
2023-10-18 09:48:57 +0100

    Add flower to the docker build and realised that it would be better to have the real variable names in the .env file, so that it can be used directly in the Celery config.

--- django/core/settings.py
+++ django/core/settings.py
@@ -138,5 +138,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis_service:6379/0")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis_service:6379/0")
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://deliberately_broken_redis_service:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://deliberately_broken_redis_service:6379/0")
