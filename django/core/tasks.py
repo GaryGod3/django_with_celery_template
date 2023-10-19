fcb7a016ca2bb0e1e3a97aa7acae8b2ff0fc4d20
Gary Godfrey <gary.god3@proton.me>
2023-10-19 14:20:02 +0100

    Add correct logging rather than prints.

--- django/core/tasks.py
+++ django/core/tasks.py
@@ -1,6 +1,10 @@
 from celery import shared_task
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
 def shared_core_task():
+    logger.info("Running shared_core_task")
     return
